554cea5ec9f43b5a0cc3825640afeb6aaa31a7d1
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-03-06 13:51:38 +0100

    añadir producto

--- src/app/_components/add-edit-product/add-edit-product.component.ts
+++ src/app/_components/add-edit-product/add-edit-product.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../../_interfaces/product.interface';
 
 @Component({
   selector: 'app-add-edit-product',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
+
+
 export class AddEditProductComponent {
 
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      stock: ['', Validators.required],
+    })
+  }
+
+  addProduct() {
+
+    console.log(this.form);
+    // const product: Product = {
+    }
 }
+
+
